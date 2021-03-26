d235cf8d1f451092eeacdcd89f93514d83096eaa
QuaBules <blazselj@gmail.com>
2021-03-26 21:24:40 +0100

    Update FS-messages-alert.user.js

--- FS-messages-alert.user.js
+++ FS-messages-alert.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name        FS
-// @namespace   FS
+// @name        Fallen Sword Message Alert
+// @namespace   Fallen Sword Message Alert
 // @include     https://fallensword.com/*
 // @version     1
 // @grant       none
@@ -11,3 +11,6 @@ if(!document.getElementById("nav-character-messages");.innerHTML.includes("(0)")
   document.getElementById("nav-box-character").style.color="black";
   document.getElementById("nav-box-character").style.background="gold";
 }
+msg=document.getElementById("nav-character-messages").innerHTML.replace("Messages","");
+chr=document.getElementById("nav-character");
+chr.innerHTML=chr.innerHTML.replace("Character","Character"+msg);
