8731f3e3d006bd8c19caede5742568ca12de194b
QuaBules <blazselj@gmail.com>
2021-03-26 22:46:36 +0100

    Update FS-messages-alert.user.js

--- FS-messages-alert.user.js
+++ FS-messages-alert.user.js
@@ -8,11 +8,11 @@
 // @grant       none
 // ==/UserScript==
 
-if(!document.getElementById("nav-character-messages").innerHTML.includes("(0)")) {
-  document.getElementById("nav-character-messages").style.color="gold";
-  document.getElementById("nav-box-character").style.color="black";
-  document.getElementById("nav-box-character").style.background="gold";
+if(!document.getElementById("nav-character-messages").innerHTML.includes("(0)")) {          //if the number of messages is not zero,
+  document.getElementById("nav-character-messages").style.color="gold";                     //change color to yellow,
+  document.getElementById("nav-box-character").style.color="black";                         //then change text color in character tab
+  document.getElementById("nav-box-character").style.background="gold";                     //and add yellow background
 }
-var msg=document.getElementById("nav-character-messages").innerHTML.replace("Messages","");
+var msg=document.getElementById("nav-character-messages").innerHTML.replace("Messages",""); //take the number of messages
 var chr=document.getElementById("nav-character");
-chr.innerHTML=chr.innerHTML.replace("Character","Character"+msg);
+chr.innerHTML=chr.innerHTML.replace("Character","Character"+msg);                           //and add it to character tab
