fe95d97fdc3f3d48d8b634960df923a1d428b65f
QuaBules <blazselj@gmail.com>
2024-10-29 21:03:55 +0100

    Update FS-messages-alert.user.js
    Changed the code just a tiny bit, no sense in seeing a zero if you don't have any messages, right?

--- FS-messages-alert.user.js
+++ FS-messages-alert.user.js
@@ -5,15 +5,16 @@
 // @match     https://www.fallensword.com/*
 // @description an attempt to alert the player to a new message
 // @author      QuaBules
-// @version     1.1
+// @version     1.2
 // @grant       none
 // ==/UserScript==
 
-if(!document.getElementById("nav-character-messages").innerHTML.includes("(0)")) {//          if the number of messages is not zero,
+if(!document.getElementById("nav-character-messages").innerText.includes("(0)")) {//          if the number of messages is not zero,
   document.getElementById("nav-character-messages").style.color="gold";//                     change color to yellow,
   document.getElementById("nav-box-character").style.color="black";//                         then change text color in character tab
   document.getElementById("nav-box-character").style.background="gold";//                     and add yellow background
+
+  var msg=document.getElementById("nav-character-messages").innerText.replace("Messages",""); //take the number of messages
+  var chr=document.getElementById("nav-character");
+  chr.innerHTML=chr.innerHTML.replace("Character","Character"+msg);//                         and add it to character tab
 }
-var msg=document.getElementById("nav-character-messages").innerHTML.replace("Messages",""); //take the number of messages
-var chr=document.getElementById("nav-character");
-chr.innerHTML=chr.innerHTML.replace("Character","Character"+msg);//                           and add it to character tab
